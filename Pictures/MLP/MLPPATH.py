471e613955756517bca61de259598ab6c33f854b
Loganius-II <132830555+Loganius-II@users.noreply.github.com>
2024-05-10 17:57:41 -0500

    Add files via upload

---
+++ Pictures/MLP/MLPPATH.py
@@ -0,0 +1,8 @@
+MLPS = ['./Pictures/MLP/logan.png',
+        './Pictures/MLP/logan2.jpg',
+        './Pictures/MLP/logan3.jpg',
+        './Pictures/MLP/logan4.jpg',
+        './Pictures/MLP/logan5.jpg',
+        './Pictures/MLP/logan6.jpg',
+        './Pictures/MLP/logan7.jpg',
+        './Pictures/MLP/logan8.jpg',]
